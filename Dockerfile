9ccc223a005ae5223cb9aa1cf1859cf827b7cb2f
m@1e0.jp <m@1e0.jp>
2023-08-13 18:18:55 +0900

    .

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,25 @@
 FROM ubuntu:22.04
 
 ENV DEBIAN_FRONTEND=noninteractive
-COPY setup/ setup/
-RUN . ./setup/first.sh
-RUN apt-get install -y open-jtalk open-jtalk-mecab-naist-jdic hts-voice-nitech-jp-atr503-m001
-RUN apt-get install -y alsa-utils
-RUN apt-get install -y pulseaudio
+RUN apt-get update
+RUN apt-get install -y pip
+RUN pip install chromadb==0.3.29
+
 RUN apt-get update \
     && apt-get install -y locales \
     && locale-gen ja_JP.UTF-8 \
     && echo "export LANG=ja_JP.UTF-8" >> ~/.bashrc
+
+RUN apt-get install -y sudo build-essential gcc-arm-linux-gnueabihf     alsa-utils pulseaudio fcitx-mozc fonts-ipafont python3-pil.imagetk
+RUN pip install customtkinter langchain openai bs4 tiktoken
+
+RUN apt-get install -y open-jtalk open-jtalk-mecab-naist-jdic hts-voice-nitech-jp-atr503-m001
+RUN apt-get install -y alsa-utils
+RUN apt-get install -y pulseaudio
+RUN apt-get autoremove -y 
+RUN apt-get clean all
+RUN rm -rf /var/cache/* /usr/local/src/* ~/.cache/pip
+
+
+COPY setup/ setup/
+RUN . ./setup/first.sh
--- setup/first.sh
+++ setup/first.sh
@@ -1,28 +1,5 @@
 #!env bash
 
-apt-get update
-
-#apt full-upgrade
-apt install -y pip
-
-pip install chromadb==0.3.29
-#pip install chromadb==0.3.29 setuptools wheel customtkinter langchain openai tiktoken streamlit bs4 openai-whisper
-
-#apt install -y libopenblas-dev libblas-dev m4 cmake python3-dev python3-yaml python3-setuptools git python3-pip python3-pil.imagetk python3-opencv fcitx-mozc fonts-ipafont
-
-#pip install --upgrade pip setuptools wheel
-#pip install setuptools wheel
-#pip install customtkinter langchain openai tiktoken streamlit bs4
-
-
-#apt-get install -y --no-install-recommends sudo build-essential gcc-arm-linux-gnueabihf pip alsa-utils pulseaudio fcitx-mozc fonts-ipafont
- apt-get install -y                         sudo build-essential gcc-arm-linux-gnueabihf     alsa-utils pulseaudio fcitx-mozc fonts-ipafont python3-pil.imagetk
-
-
-apt-get autoremove -y 
-apt-get clean all
-pip install customtkinter langchain openai bs4 tiktoken
-rm -rf /var/cache/* /usr/local/src/* ~/.cache/pip
 
 
 # Julius
@@ -43,4 +20,3 @@ rm -rf /var/cache/* /usr/local/src/* ~/.cache/pip
     sudo make install
 )
 
-:
